1f71e9ffdfeeab37a8c475bc64ca13cdade33b43
Jung-Woo-Sik <o0o0@cs-cnu.org>
2021-08-02 15:16:07 +0900

    delete

--- truffle_01/test/hello_token.js
+++
@@ -1,9 +0,0 @@
-const HelloToken = artifacts.require("./HelloToken.sol");
-
-contract('HelloToken',(accounts) =>{
-	it("hello2",() => {
-	return HelloToken.deployed().then(instance => {
-	instance.hello2().then(result => console.log(result))
-	});
-	});
-});
